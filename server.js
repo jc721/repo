7882f85937ab01625bd2bd8faa813c3a77d00e20
JC <jc@MacBook-Pro-di-Jc.local>
2017-12-10 19:21:40 +0100

    bower

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ fs.readFile('./src/index.html', function (err, html) {
         response.end();  
     }).listen(process.env.PORT || 8000);
 });
+
+var express = require('express');
+var app = express();
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/bower_components', express.static(__dirname + '/../bower_components'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/partials', express.static(__dirname + '/partials'));
+
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+});
